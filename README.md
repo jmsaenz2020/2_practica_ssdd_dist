46506eae74043d052a6178845ad869b2be4f17e5
jsmnlsnz <jsmnlsnz@optiplex9020.local>
2025-11-13 17:47:23 +0100

    Tests in modules

---
+++ README.md
@@ -0,0 +1,7 @@
+# Practica 2
+
+```
+$ cd src/
+$ go build .
+$ ./2_practica_ssdd_dist
+```
--- src/taller/taller_test.go
+++ src/taller/taller_test.go
@@ -1,24 +1,27 @@
-package taller_test
+package taller
 
 import (
   "testing"
-  "p2/main"
 )
 
-var defaults Taller
-defaults.CrearMecanico("Pepe", 0, 0)
-c := Cliente{Id: 1, Nombre: "Laura", Telefono: 1, Email: "laura27@mail.com"}
-v := Vehiculo{Matricula: 1234, Marca: "Toyota", Modelo: "Camry", FechaEntrada: "14-04-2009", FechaSalida: "19-04-2009"}
-i := Incidencia{Id: 1, Tipo: 1, Prioridad: 1, Descripcion: "Luna delantera rota", Estado: 1}
-v.CrearIncidencia(i)
-c.CrearVehiculo(v)
-v = Vehiculo{Matricula: 1235, Marca: "Toyota", Modelo: "Camry", FechaEntrada: "14-04-2009", FechaSalida: "19-04-2009"}
-c.CrearVehiculo(v)
-defaults.CrearCliente(c)
-defaults.AsignarPlaza(c.Vehiculos[0])
-defaults.AsignarPlaza(c.Vehiculos[1])
+func getDefaults()(Taller){
+  var defaults Taller
+  defaults.CrearMecanico("Pepe", 0, 0)
+  c := Cliente{Id: 1, Nombre: "Laura", Telefono: 1, Email: "laura27@mail.com"}
+  v := Vehiculo{Matricula: 1234, Marca: "Toyota", Modelo: "Camry", FechaEntrada: "14-04-2009", FechaSalida: "19-04-2009"}
+  i := Incidencia{Id: 1, Tipo: 1, Prioridad: 1, Descripcion: "Luna delantera rota", Estado: 1}
+  v.CrearIncidencia(i)
+  c.CrearVehiculo(v)
+  v = Vehiculo{Matricula: 1235, Marca: "Toyota", Modelo: "Camry", FechaEntrada: "14-04-2009", FechaSalida: "19-04-2009"}
+  c.CrearVehiculo(v)
+  defaults.CrearCliente(c)
+  defaults.AsignarPlaza(c.Vehiculos[0])
+  defaults.AsignarPlaza(c.Vehiculos[1])
+  
+  return defaults
+}
 
 func Benchmark(b *testing.B){
-  t := defaults
+  t := getDefaults()
   t.Estado()
 }
